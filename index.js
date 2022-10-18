634a43eb3d5594b148fc3f3aea8108fc6a7d924e
DeveloperZER0 <w-piwowar@wp.pl>
2022-10-18 13:29:28 +0200

    test

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const prisma = new PrismaClient()*/
 const express = require('express')
 var cors = require('cors')
 
+const pgp = require('pg-promise')(/* options */)
+const db = pgp(env("DATABASE_URL"))
+
 const app = express()
 const PORT = 4000
 app.listen(PORT, () =>
@@ -10,10 +13,18 @@ app.listen(PORT, () =>
 )
 
 app.use(cors())
+app.get('/', async (req,res) => {
+  res.send('API projektu SPOTTED na potrzebę konkursu hackheroes')
+})
 app.get('/markers', async (req, res) => {
-    //const allMarkers = await prisma.post.findMany()
-    //res.json(allMarkers)
-    res.send('Siema')
+  try {
+    const markers = await db.any('SELECT * FROM Post', [true]);
+    res.json(markers)
+  } 
+  catch(e) {
+    res.sendStatus(500)
+  }
+
 })
 app.post('/receive', (req, res) => {
     console.log(req.body);
