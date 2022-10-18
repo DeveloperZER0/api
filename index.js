5fe756506d92e2472c9b03f95bcc8f367d87d9f9
DeveloperZER0 <w-piwowar@wp.pl>
2022-10-18 13:49:50 +0200

    test prisma

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/*const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()*/
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const express = require('express')
 var cors = require('cors')
-
-const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgres://dev:5HGpd5hVbUmK9vKdlLGmrtLmEgGZEr1C@dpg-cd2h8lqen0hgnc904h30-a.frankfurt-postgres.render.com/hackheroesdev')
 
 const app = express()
 const PORT = 4000
@@ -18,8 +15,8 @@ app.get('/', async (req,res) => {
 })
 app.get('/markers', async (req, res) => {
   try {
-    const markers = await db.any('SELECT * FROM Post', [true]);
-    res.json(markers)
+    const allMarkers = await prisma.post.findMany()
+    res.json(allMarkers)
   } 
   catch(e) {
     console.log('Error: ' + e)
